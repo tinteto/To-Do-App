647ca661b0b7c997889e93718b4dd58a9a275415
tinteto <radosttina.dimitrova@gmail.com>
2025-05-12 15:24:47 +0300

    Add font awesome icons

--- client/src/components/ToDoItem.jsx
+++ client/src/components/ToDoItem.jsx
@@ -1,14 +1,23 @@
 import styles from './TodoItem.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faEdit, faSyncAlt, faRedo } from '@fortawesome/free-solid-svg-icons';
 
-export default function ToDoItem ({_id, title, isCompleted, onStatusChange }) {
+
+export default function ToDoItem ({_id, text, isCompleted, onStatusChange, onDeleteTask }) {
     return (
 <>
 <li>
   <div className={styles.text}>
-  <p className={isCompleted ? styles.textCompleted : styles.textToDo}> {title} </p>
+  <p className={isCompleted ? styles.textCompleted : styles.textToDo}> {text} </p>
   <p className={isCompleted ? styles['isCompleted'] : styles['toDo']}> {isCompleted ? "done" : "pending"} </p>
   </div>
-  <button onClick={() => onStatusChange(_id)}>Change Status</button>
+
+  <div className={styles.buttons}>
+  <button onClick={() => onStatusChange(_id)}>
+  <FontAwesomeIcon icon={faRedo} /> </button>
+  <button onClick={() => onDeleteTask(_id)}>
+  <FontAwesomeIcon icon={faTrash} /> </button>
+  </div>
 </li>
 </>
     )
